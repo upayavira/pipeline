65b4cf00b80aa7586493b89ec9ad6d02e7ebf8c6
Upayavira <uv@odoko.co.uk>
2011-12-10 23:32:47 +0000

    Another asset handler test

--- src/test/java/org/odoko/pipeline/handlers/UniqueUriAssetHandlerTest.java
+++ src/test/java/org/odoko/pipeline/handlers/UniqueUriAssetHandlerTest.java
@@ -2,7 +2,9 @@ package org.odoko.pipeline.handlers;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.odoko.pipeline.handlers.AssetHandler;
@@ -12,12 +14,17 @@ import org.odoko.pipeline.model.Asset;
 public class UniqueUriAssetHandlerTest {
 
 	private static final String DB_FILE = "target/database.tmp";
-	@Test
-	public void testUniqueAssetsNotRepeated() throws IOException {
+
+	@Before
+	public void setUp() {
 		File db = new File(DB_FILE);
 		if (db.exists()) {
 			db.delete();
 		}
+	}
+
+	@Test
+	public void testUniqueAssetsNotRepeated() throws IOException {
 		AssetHandler handler = new UniqueUriAssetHandler(DB_FILE);
 		Asset asset1 = new Asset();
 		Asset asset2 = new Asset();
@@ -34,5 +41,27 @@ public class UniqueUriAssetHandlerTest {
 			handler.nextAsset();
 		}
 		assertEquals("Handler should only have one asset, as the two URIs are the same", 1, i);
+	}
+	
+	@Test
+	public void testUniqueAssetsNotRepeatedWhenLoggedInDatabase() throws IOException {
+		File db = new File(DB_FILE);
+		PrintWriter writer = new PrintWriter(db);
+		writer.println("http://app.odoko.co.uk/unique");
+		writer.close();
+
+		AssetHandler handler = new UniqueUriAssetHandler(DB_FILE);
+		Asset asset1 = new Asset();
+		asset1.setValue("ASSET1");
+		asset1.setUri("http://app.odoko.co.uk/unique");
+		handler.addAsset(asset1);
+		
+		int i=0;
+		while (handler.hasNext()) {
+			i++;
+			handler.nextAsset();
+		}
+		assertEquals("Handler should have no assets set, as the asset has been seen before", 0, i);
+
 	}
 }
